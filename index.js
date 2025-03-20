4b5f8dfe6353d0b229068e0a78b4ecbfe3040131
Arsh369 <playgames1797@gmail.com>
2025-03-20 17:21:42 +0530

    v

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/api/admin", adminRoutes)
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
-  app.use("/b", (req, res) => {
+  app.use("/", (req, res) => {
     res.send("Book Store Server is running!");
   });
 }
